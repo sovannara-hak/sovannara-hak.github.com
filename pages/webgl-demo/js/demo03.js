5f906ce7cbc5ab8592ebb93fa71dd8a6a34468dc
Sovannara Hak <sovannara-hak@users.noreply.github.com>
2016-01-20 17:11:09 +0100

    WebGL-demo: Complete demo 03

--- pages/webgl-demo/js/demo03.js
+++ pages/webgl-demo/js/demo03.js
@@ -44,9 +44,42 @@ function initVertexBuffer(){
 var pMatrix = mat4.create();
 var mvMatrix = mat4.create();
 
+var mvMatrixStack = [];
+
 function setMatrixUniforms(){
     gl.uniformMatrix4fv(pMatrixUniform, false, pMatrix);
     gl.uniformMatrix4fv(mvMatrixUniform, false, mvMatrix);
+}
+
+var rotationSpeed = degToRad(50);
+var currentTriangleAngle = degToRad(0);
+var lastTime = 0;
+
+function mvPushMatrix(){
+    var copy = mat4.create();
+    mat4.copy(mvMatrix, copy);
+    mvMatrixStack.push(copy);
+}
+
+function mvPopMatrix(){
+    if(mvMatrixStack.length == 0){
+        throw "Invalid popMatrix";
+    }
+    mvMatrix = mvMatrixStack.pop();
+}
+
+function degToRad(d) {
+    return d * Math.PI / 180;
+}
+
+function tick(){
+    requestAnimationFrame(initTime);
+    requestAnimationFrame(draw);
+}
+
+function initTime(now){
+    now *= 0.001;
+    lastTime = now;
 }
 
 function startGL(){
@@ -64,43 +97,63 @@ function startGL(){
     //and then we call the draw function
     Promise.all([vertexShaderPromise, fragmentShaderPromise])
         .then(createProgram)
-        .then(draw);
+        .then(tick);
 }
 
-function draw(){
-        //set the vertices data
-        initVertexBuffer();
-        initShaderAttrib();
+function draw(now){
+    now *= 0.001;
+    var deltaTime = now - lastTime;
+    lastTime = now;
 
-        gl.clearColor(0.0, 0.0, 0.0, 1.0);
-        gl.clear(gl.COLOR_BUFFER_BIT);
+    //set the vertices data
+    initVertexBuffer();
+    initShaderAttrib();
 
-        gl.viewport(0.0, 0.0, gl.viewportWidth, gl.viewportHeight);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
 
-        mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, pMatrix);
-        mat4.identity(mvMatrix);
-        mat4.translate(mvMatrix, mvMatrix, [-0.5, 0.0, 0.0]);
+    gl.viewport(0.0, 0.0, gl.viewportWidth, gl.viewportHeight);
 
-        setMatrixUniforms();
+    mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, pMatrix);
+    mat4.identity(mvMatrix);
 
-        //Only one program, so we use the first one in the list
-        gl.useProgram(programList[0]);
+    setMatrixUniforms();
 
-        gl.bindBuffer(gl.ARRAY_BUFFER, positionsBufferObject);
-        //enable the first attribute of the vertex shader
-        gl.enableVertexAttribArray(vertexPositionAttribute);
+    //Only one program, so we use the first one in the list
+    gl.useProgram(programList[0]);
 
-        //enable the color attribute
-        gl.enableVertexAttribArray(vertexColorAttribute);
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionsBufferObject);
+    //enable the first attribute of the vertex shader
+    gl.enableVertexAttribArray(vertexPositionAttribute);
 
-        //index, number of values, type, false, 0 space between values, offset
-        //vertexData: [vertex1_pos, vertex2_pos, vertex3_pos, 
-        gl.vertexAttribPointer(vertexPositionAttribute, 4, gl.FLOAT, false, 0, 0);
-        //             vertex1_color, vertex2_color, vertex3_color]
-        gl.vertexAttribPointer(vertexColorAttribute, 4, gl.FLOAT, false, 0, 48);
+    //enable the color attribute
+    gl.enableVertexAttribArray(vertexColorAttribute);
 
-        gl.drawArrays(gl.TRIANGLES, 0, 3);
+    //index, number of values, type, false, 0 space between values, offset
+    //vertexData: [vertex1_pos, vertex2_pos, vertex3_pos, 
+    gl.vertexAttribPointer(vertexPositionAttribute, 4, gl.FLOAT, false, 0, 0);
+    //             vertex1_color, vertex2_color, vertex3_color]
+    gl.vertexAttribPointer(vertexColorAttribute, 4, gl.FLOAT, false, 0, 48);
 
+    //Draw a rotating triangle
+    mvPushMatrix();
+    currentTriangleAngle += rotationSpeed * deltaTime
+    mat4.translate(mvMatrix, mvMatrix, [-0.5, 0.0, 0.0]);
+    mat4.rotate(mvMatrix, mvMatrix, currentTriangleAngle, [0, 1, 0]);
+
+    setMatrixUniforms();
+
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
+    mvPopMatrix();
+
+    //Draw a fixed triangle
+    mvPushMatrix();
+    mat4.translate(mvMatrix, mvMatrix, [0.5, 0.0, 0.0]);
+    setMatrixUniforms();
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
+    mvPopMatrix();
+
+    requestAnimationFrame(draw);
 }
 
 function demo03_main(canvas_name){
